b77e4ff1b71bb1cfda68d7679aff2138b27b10c2
Peanutt42 <p3anutt42@gmail.com>
2024-04-03 02:23:47 +0200

    added scale and seed parameters

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use noise::{Fbm, NoiseFn, Perlin};
 use image::RgbImage;
+use indicatif::ParallelProgressIterator;
+use noise::{Fbm, NoiseFn, Perlin};
 use rand::Rng;
 use rayon::prelude::*;
-use indicatif::ParallelProgressIterator;
 
 mod color;
 use color::Color;
@@ -19,12 +19,12 @@ fn noise_to_color(noise: f64, color1: Color, color2: Color, bands: f64, band_wid
 // returns percentage (150% - 50%) for how much a sample point should add to the final color
 // avarage between offset = -0.5 and offset = 0.5 is 100%
 fn get_sample_importance(x_offset: f64, y_offset: f64) -> f64 {
-	let offset = (x_offset*x_offset + y_offset*y_offset).sqrt();
+	let offset = (x_offset * x_offset + y_offset * y_offset).sqrt();
 	-2.0 * offset + 1.5
 }
 
-fn generate(filepath: &str, width: usize, height: usize, samples: usize) {
-	let fbm = Fbm::<Perlin>::new(0);
+fn generate(filepath: &str, width: usize, height: usize, samples: usize, scale: f64, seed: u32) {
+	let fbm = Fbm::<Perlin>::new(seed);
 
 	let mut image = vec![Color::default(); width * height];
 	image
@@ -35,21 +35,24 @@ fn generate(filepath: &str, width: usize, height: usize, samples: usize) {
 			let mut rng = rand::thread_rng();
 			for (x, output) in row.iter_mut().enumerate() {
 				for _ in 0..samples {
+					let x = x as f64 * scale;
+					let y = y as f64 * scale;
 					let x_offset = rng.gen_range(-0.5..0.5);
 					let y_offset = rng.gen_range(-0.5..0.5);
 					let sample_importance = get_sample_importance(x_offset, y_offset);
 					let value = fbm.get([
-						((x as f64 + x_offset) / width as f64) * 0.64,
-						((y as f64 + y_offset) / height as f64) * 0.64]);
+						(x + x_offset) / width as f64,
+						(y + y_offset) / height as f64,
+					]);
 					let color = noise_to_color(
 						value,
-						Color{ r: 115.0 / 255.0, g: 0.0, b: 1.0},
-						Color{ r: 0.0, g: 0.0, b: 0.0 },
-						50.0,
-						0.00125,
+						Color { r: 1.0, g: 1.0, b: 1.0 },
+						Color { r: 0.0, g: 0.0, b: 0.0 },
+						100.0,
+						0.0018,
 					);
 					*output += color * sample_importance * (1.0 / samples as f64);
-				}			
+				}
 			}
 		});
 
@@ -63,8 +66,8 @@ fn generate(filepath: &str, width: usize, height: usize, samples: usize) {
 				image::Rgb([
 					(color.r * 255.0) as u8,
 					(color.g * 255.0) as u8,
-					(color.b * 255.0) as u8
-				])
+					(color.b * 255.0) as u8,
+				]),
 			);
 		}
 	}
@@ -72,10 +75,10 @@ fn generate(filepath: &str, width: usize, height: usize, samples: usize) {
 }
 
 fn main() {
-	generate("output.png", 2560, 1440, 200);
+	generate("output.png", 2560, 1440, 200, 0.75, 0);
 }
 
 #[test]
 fn generate_preview() {
-	generate("preview.png", 800, 500, 200);
+	generate("preview.png", 800, 500, 25, 0.75, 0);
 }
